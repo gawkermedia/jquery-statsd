b381de954f8c33568de6d928fe4fbdde5be6cdc1
Jozsef Kozma <joco@gawker.com>
2013-09-09 15:06:59 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+jQuery.statsd
+=============
+plugin measuring and reporting times spent on frontend
+
+This plugin can measure and report times spent on arbitrary items in your front-end code. It is backed by Navigation Timing API which you can find in modern browsers. It can report your data to your statsd installation, however, backend code needed for reporting is not included.
+
+Installation
+------------
+You just need to add the file *jquery.statsd.js* to your JS stack and include contents of *jquery.statsd.css* in your stylesheet.
+
+Usage
+-----
+There are two modes of the plugin, first you need to initialize it, any subsequent calls will be considered as measurement points in your code.
+
+### Init
+```
+$.statsd({
+    displayReport: true,
+    statsdUrl: '/stats/mark',
+    reportEvents: [
+		{
+			event: 'responseEnd',
+			message: 'Response end',
+			reporting: false
+		},
+		{
+			event: 'loadEventEnd',
+			message: 'Load end',
+			reporting: true
+		}
+    ],
+    appendParams: function (params) {
+        return params;
+    })
+});
+```
+#### Options:
+- displayReport: boolean field, setting it true will render a report of all timings on your page too.
+- statsdUrl: your API endpoint path for reporting times to statsd.
+- reportEvents: data about these Navigation Timing events will be included in the query sent to statsd.
+- appendParams: a wrapper method for appending arbitrary values you need in your own environment to the query string of the statsd API call.
+
+### Measurement
+```
+$.statsd(message, reporting);
+```
+#### Parameters
+- message: An arbitrary message marking your code.
+- reporting: Boolean value, only records with reporting set to *true* will be reported to statsd later.
+
+### Reporting to statsd
+Reporting happens automatically on window.onLoad in case you set up *statsdUrl* during init. Plugin is sending data about the Navigation Timing events preconfigured in *reportEvents* as well as data about the measurement points you set up in your own code with *reporting* set to true.
---
+++ jquery.statsd.js
@@ -0,0 +1,194 @@
+/**
+ * jQuery plugin measuring and reporting time spent with specific things in the browser
+ *
+ * @author Jozsef Kozma
+ */
+
+'use strict';
+
+/*global define, window, jQuery */
+
+(function (factory) {
+	if (typeof define === 'function' && define.amd) {
+		define(['lib/jquery'], factory);
+	} else {
+		factory(jQuery);
+	}
+}(function ($) {
+	if (!window.performance) {
+		return null;
+	}
+
+	var settings,
+		ticks = [];
+
+	$.statsd = function (options) {
+
+		// Default settings you can override in options
+		settings = $.extend({
+			displayReport: false,
+			reportEvents: [
+				{
+					event: 'responseEnd',
+					message: 'Response end',
+					reporting: false
+				},
+				{
+					event: 'domContentLoadedEventEnd',
+					message: 'DOM content loaded',
+					reporting: false
+				},
+				{
+					event: 'domComplete',
+					message: 'DOM complete',
+					reporting: false
+				},
+				{
+					event: 'loadEventEnd',
+					message: 'Load end',
+					reporting: true
+				}
+			],
+			appendParams: function (params) {
+				return params;
+			}
+		}, options);
+
+		var timing = window.performance.timing,
+
+			ticks = window.statsdTicks,
+
+			reportRendered = false,
+
+			/**
+			 * Will render a single line of timing report on your page.
+			 * @param  {object} tick Data of the timing to be rendered.
+			 */
+			appendTick = function (tick) {
+				var reporting = (tick.reporting === true) ? 'X' : '';
+
+				$('.advanced-timing-table').append(
+					'<tr><td class="text-right">' + (tick.ms - timing.navigationStart) + ' ms</td><td class="text-right">' + (tick.ms - timing.responseEnd) + ' ms</td><td>' + tick.message + '</td><td>' + reporting + '</td></tr>'
+				);
+			},
+
+			/**
+			 * Will render the whole HTML report on your page.
+			 */
+			renderReport = function () {
+				var navStartToLoadEnd = timing.loadEventEnd - timing.navigationStart,
+					i,
+					l = ticks.length;
+
+				$('body').prepend('<div id="timing-report" class="proxima">' +
+					navStartToLoadEnd +	' ms - Navigation Start to Page Load End <br /><hr />' +
+					'<table class="advanced-timing-table"><thead><tr><th>Clock</th><th>Relative</th><th>Name</th><th>Reporting</th></tr></thead></table></div>');
+
+				for (i = 0; i < l; i++) {
+					appendTick(ticks[i]);
+				}
+
+				reportRendered = true;
+			},
+
+			/**
+			 * Records a new tick.
+			 * @param  {object} tick Timing data.
+			 */
+			newTick = function (tick) {
+				ticks.push(tick);
+
+				if (reportRendered === true) {
+					appendTick(tick);
+				}
+			},
+
+			/**
+			 * Turns data recorded into query string params.
+			 * @param  {array} ticks Timing data to be sent to statsd.
+			 * @return {array} Array of strings in key=value pattern.
+			 */
+			prepareParams = function (ticks) {
+				var i,
+					l = ticks.length,
+					retval = [];
+
+				for (i = 0; i < l; i++) {
+					retval.push(ticks[i].message.toLowerCase().replace(/\s/g, '_') + '=' + (ticks[i].ms - timing.responseEnd));
+				}
+
+				return retval;
+			},
+
+			/**
+			 * Sends timing data to your statsd API endpoint.
+			 */
+			reportTicks = function () {
+				var navStartToLoadEnd = timing.loadEventEnd - timing.navigationStart,
+					ticksToReport = (function () {
+						// only the ones with reporting=true
+						var filteredTicks = [],
+							i,
+							l = ticks.length;
+
+						for (i = 0; i < l; i++) {
+							if (ticks[i].reporting === true) {
+								filteredTicks.push(ticks[i]);
+							}
+						}
+
+						return filteredTicks;
+					}()),
+					params = settings.appendParams(prepareParams(ticksToReport));
+
+				$.getScript(settings.statsdUrl + '?' + params.join('&'));
+			};
+
+		$(window).on('load', function () {
+			window.setTimeout(function () {
+				(function () {
+					// sends ticks for Navigation Timing API events configured
+					var i,
+						l = settings.reportEvents.length;
+
+					for (i = 0; i < l; i++) {
+						newTick({
+							ms: timing[settings.reportEvents[i].event],
+							message: settings.reportEvents[i].message,
+							reporting: settings.reportEvents[i].reporting
+						});
+					}
+				}());
+
+				// sorting ticks by time
+				ticks = ticks.sort(function (a, b) {
+					var retval = 1;
+
+					if (a.ms < b.ms) {
+						retval = -1;
+					}
+
+					return retval;
+				});
+
+				if (settings.displayReport === true) {
+					renderReport();
+				}
+
+				if (settings.statsdUrl !== undefined) {
+					reportTicks();
+				}
+			}, 0);
+		});
+
+		// after a single initialization, rewrite $.statsd with the lone public method
+		$.statsd = function (message, reporting) {
+			newTick({
+				ms: Date.now(),
+				message: message,
+				reporting: reporting || false
+			});
+		};
+
+	};
+}));
